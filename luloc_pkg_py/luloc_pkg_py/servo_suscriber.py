8427781de870c0218b541a908bf1d69769004a48
RaulGarviCascos <raul.garvi.cascos@gmail.com>
2026-05-27 15:21:59 +0200

    servo por ros2

---
+++ luloc_pkg_py/luloc_pkg_py/servo_suscriber.py
@@ -0,0 +1,54 @@
+import os
+os.environ['GPIOZERO_PIN_FACTORY'] = 'lgpio'
+from gpiozero import Servo, Button
+from time import sleep, time
+import sys
+from rclpy.node import Node
+from std_msgs.msg import Float32
+from enum import IntEnum
+
+
+servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+sensor_inf = Button(17, pull_up=False) 
+sensor_sup = Button(27, pull_up=False)
+
+
+vel = 0.3
+
+class Servo(Node):
+    def __init__(self):
+        super().__init__("servo_suscriber")
+        self.subscription_ = self.create_subscription(
+            Float32,
+            "cmd_vel",
+            self.calback_servo,
+            10
+        )
+        self.get_logger().info("Listener servo has been started")
+
+    def calback_servo(self, msg: Float32):
+        puls_inf = sensor_inf.is_pressed
+        puls_sup = not sensor_sup.is_pressed
+        vel = float(msg.Data)
+        if not puls_inf or not puls_sup:
+            if vel == 0:
+                servo.detach()
+            servo.value = vel
+        else:
+            servo.detach()
+ 
+   
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Servo()
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+
+if __name__ == "__main__":
+    main()
